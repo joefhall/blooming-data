9d6c5c8ae707106309a37bccb7766c953d71a02a
Joe Hall <joefhall@gmail.com>
2018-07-10 13:43:16 +0000

    Add pie positions to flowers config

--- js/flowers.js
+++ js/flowers.js
@@ -4,131 +4,155 @@ flowers = [
     keyColour: '',
     start: 5.1,
     stop: 12.1,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 725 * 1.11,
+        top: 345 * 1.11,
+      },
+      radius: 430 * 1.11
+    }
   },
   {
     description: 'White daffodil',
     keyColour: '',
     start: 13.4,
     stop: 21.2,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 616 * 1.11,
+        top: 282 * 1.11,
+      },
+      radius: 240 * 1.11
+    }
   },
   {
     description: 'Yellow large flower with stem',
     keyColour: '',
     start: 21.4,
     stop: 30.7,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 610 * 1.11,
+        top: 382 * 1.11,
+      },
+      radius: 333 * 1.11
+    }
   },
   {
     description: 'Pink rose',
     keyColour: '',
     start: 31.2,
     stop: 39.8,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 685 * 1.11,
+        top: 345 * 1.11,
+      },
+      radius: 260 * 1.11
+    }
   },
   {
     description: 'Yellow rose flat',
     keyColour: '',
     start: 40.2,
     stop: 48,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 630 * 1.11,
+        top: 375 * 1.11,
+      },
+      radius: 270 * 1.11
+    }
   },
   {
     description: 'Bright pink rose',
     keyColour: '',
     start: 48.5,
     stop: 56.9,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 695 * 1.11,
+        top: 285 * 1.11,
+      },
+      radius: 300 * 1.11
+    }
   },
   {
     description: 'White rose',
     keyColour: '',
     start: 57.3,
     stop: 66.3,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 705 * 1.11,
+        top: 450 * 1.11,
+      },
+      radius: 210 * 1.11
+    }
   },
   {
     description: 'Red-orange rose',
     keyColour: '',
     start: 66.8,
     stop: 76.5,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 570 * 1.11,
+        top: 235 * 1.11,
+      },
+      radius: 430 * 1.11
+    }
   },
   {
     description: 'Yellow flower large w/ stamens',
     keyColour: '',
     start: 77.2,
     stop: 88,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 800,
+        top: 400,
+      },
+      radius: 480
+    }
   },
   {
     description: 'Pink rose with foliage',
     keyColour: '',
     start: 88.7,
     stop: 101.5,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 630,
+        top: 360,
+      },
+      radius: 340
+    }
   },
   {
     description: 'Sticky pink',
     keyColour: '',
     start: 102.5,
     stop: 114.3,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 800,
+        top: 445,
+      },
+      radius: 540
+    }
   },
   {
     description: 'Yellow rose',
     keyColour: '',
     start: 115,
     stop: 125.5,
-    pieCentre: {
-      left: 120,
-      top: 120,
-    },
-    pieRadius: 100
+    pie: {
+      centre: {
+        left: 735,
+        top: 475,
+      },
+      radius: 450
+    }
   },
 ];
