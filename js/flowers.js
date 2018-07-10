4519d2f29196f2a5f255f9cf257ec89e1fa5b76e
Joe Hall <joefhall@gmail.com>
2018-07-10 12:29:09 +0000

    Add flowers config

---
+++ js/flowers.js
@@ -0,0 +1,134 @@
+flowers = [
+  {
+    description: 'Yellow large flower',
+    keyColour: '',
+    start: 5.1,
+    stop: 12.1,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'White daffodil',
+    keyColour: '',
+    start: 13.4,
+    stop: 21.2,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Yellow large flower with stem',
+    keyColour: '',
+    start: 21.4,
+    stop: 30.7,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Pink rose',
+    keyColour: '',
+    start: 31.2,
+    stop: 39.8,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Yellow rose flat',
+    keyColour: '',
+    start: 40.2,
+    stop: 48,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Bright pink rose',
+    keyColour: '',
+    start: 48.5,
+    stop: 56.9,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'White rose',
+    keyColour: '',
+    start: 57.3,
+    stop: 66.3,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Red-orange rose',
+    keyColour: '',
+    start: 66.8,
+    stop: 76.5,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Yellow flower large w/ stamens',
+    keyColour: '',
+    start: 77.2,
+    stop: 88,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Pink rose with foliage',
+    keyColour: '',
+    start: 88.7,
+    stop: 101.5,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Sticky pink',
+    keyColour: '',
+    start: 102.5,
+    stop: 114.3,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+  {
+    description: 'Yellow rose',
+    keyColour: '',
+    start: 115,
+    stop: 125.5,
+    pieCentre: {
+      left: 120,
+      top: 120,
+    },
+    pieRadius: 100
+  },
+];
